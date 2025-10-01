2712808b07dbce71ee55583f652b745df650fb5b
navdya <it24100652@my.sliit.lk>
2025-10-02 01:26:42 +0530

    IT24100652.R

---
+++ Lab Sheet 09/IT24100652.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\User\\Desktop\\IT24100652_Lab09")
+getwd()
+
+
+# Set seed for reproducibility
+set.seed(123)
+# Part (i) Generate random sample
+sample_size <- 25
+mu <- 45
+sigma <- 2
+baking_times <- rnorm(sample_size, mean = mu, sd = sigma)
+print(baking_times)
+# Part (ii) Hypothesis test
+# H0: mean = 46
+# H1: mean < 46
+
+t_test_result <- t.test(baking_times, mu = 46, alternative = "less")
+print(t_test_result)
